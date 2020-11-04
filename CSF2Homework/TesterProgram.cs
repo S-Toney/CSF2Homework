65e33fc6328e259288d0f4750b38a264e5049fdf
Stacy Toney <stacy.toney@protonmail.com>
2020-11-03 21:22:15 -0600

    Test1 created, coded, & successful

--- CSF2Homework/TesterProgram.cs
+++ CSF2Homework/TesterProgram.cs
@@ -11,8 +11,33 @@ namespace CSF2Homework
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+            s1.FirstName = "Helen";
+            s1.LastName = "Keller";
+            s1.Id = "B&D1";
+            s1.Gpa = 3.8f;
+            Console.WriteLine(s1);
 
+            Vehicle v1 = new Vehicle();
+            v1.Year = 1999;
+            v1.Make = "Jeep";
+            v1.Model = "Wrangler";
+            v1.Weight = 3760;
+            Console.WriteLine(v1);
 
+            Login l1 = new Login();
+            l1.UserName = "hKeller";
+            l1.Password = "hKB&D1";
+            Console.WriteLine(l1);
+
+            ContactInfo c1 = new ContactInfo();
+            c1.StreetAddress = "1300 S Montgomery Ave.";
+            c1.City = "Sheffield";
+            c1.State = "Alabama";
+            c1.Zip = "35660";
+            c1.Phone = "(256) 386-4196";
+            c1.Email = "itsupport@helenkeller.com";
+            Console.WriteLine(c1);
         }
 
     }
