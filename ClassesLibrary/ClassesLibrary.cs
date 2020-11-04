da01ca16306648657524291a21396d3a059e0d13
Stacy Toney <stacy.toney@protonmail.com>
2020-11-03 21:20:40 -0600

    ContactInfor class created and coded

--- ClassesLibrary/ClassesLibrary.cs
+++ ClassesLibrary/ClassesLibrary.cs
@@ -147,5 +147,73 @@ namespace ClassesLibrary
         }
     }//end Login
 
+    public class ContactInfo
+    {
+        //fields
+        private string _streetAddress;
+        private string _city;
+        private string _state;
+        private string _zip;
+        private string _phone;
+        private string _email;
+
+        //prpoerties
+        public string StreetAddress
+        {
+            get { return _streetAddress; }
+            set { _streetAddress = value; }
+        }
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+        public string Zip
+        {
+            get { return _zip; }
+            set { _zip = value; }
+        }
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        //ctors
+        //FULLY QUALIFIED CTOR
+        public ContactInfo(string streetAddress, string city, string state, string zip, string phone, string email)
+        {
+            StreetAddress = streetAddress;
+            City = city;
+            State = state;
+            Zip = zip;
+            Phone = phone;
+            Email = email;
+        }
+
+        //DEFAULT CTOR
+        public ContactInfo() { }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format("\n-*-*-*- CONTACTS -*-*-*-\n\n" +
+                $"Address: {StreetAddress}\n" +
+                $"\t {City}, {State} {Zip}\n" +
+                $"Phone: {Phone}\n" +
+                $"Email: {Email}\n\n");
+        }
+
+    }//end ContactInfo
 }
 
